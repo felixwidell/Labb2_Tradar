1f430f9042b3140dacc09176deed02f3379725a9
Felix Widell <felix.widell@gmail.com>
2024-03-05 16:17:31 +0100

    Add project files.

---
+++ Car.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb2_Tradar
+{
+    public class Car
+    {
+        public string Name { get; set; }
+        public double Speed { get; set; } = 120;
+        public double KmTravelled { get; set; } = 0;
+        public bool HaventFinished { get; set; } = true;
+
+        public void DriveCar(ManualResetEvent myEvent)
+        {
+            int RandomeventTimer = 0;
+            int SecondsPassed = 0;
+            double MetersTravelled = 0;
+            double MeterPerSecConvertNumber = 0.2777;
+
+            do
+            {
+                double MetersPerSecond = Speed * MeterPerSecConvertNumber;
+                MetersTravelled = MetersTravelled + MetersPerSecond;
+                KmTravelled = MetersTravelled / 1000;
+                RandomeventTimer++;
+                SecondsPassed++;
+               
+
+                if (RandomeventTimer == 30)
+                {
+                    RandomEvent();
+                    RandomeventTimer = 0;
+                }
+
+                Thread.Sleep(1000);
+            } while (KmTravelled < 10);
+
+            Console.WriteLine($"{Name} has reached the finish line!");
+            HaventFinished = false;
+            myEvent.Set();
+        }
+
+        public void RandomEvent()
+        {
+            Random rnd = new Random();
+            int RandomNumb = rnd.Next(1, 50);
+
+            if(RandomNumb == 1)
+            {
+                Console.WriteLine($"{Name} is out of gas, refuelling for 30 seconds");
+                Thread.Sleep(30000);
+            }
+            else if (RandomNumb == 2 || RandomNumb == 3)
+            {
+                Console.WriteLine($"{Name} is taking a 20 second pit stop to change tires");
+                Thread.Sleep(20000);
+            }
+            else if (RandomNumb >= 4 && RandomNumb <= 8) 
+            {
+                Console.WriteLine($"{Name} got a bird on their windshield and is taking 10 seconds to wipe it off");
+                Thread.Sleep(10000);
+            }
+            else if (RandomNumb >= 9 && RandomNumb <= 19)
+            {
+                Console.WriteLine($"{Name}s engine is overheating and is driving 1 km/h slower");
+                Speed = Speed - 1;
+            }
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿namespace Labb2_Tradar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int a = 2; a >= 0; a--)
+            {
+                Console.WriteLine(a+1);
+                System.Threading.Thread.Sleep(1000);
+            }
+            Console.WriteLine("Go!");
+
+            var Mustang = new Car();
+            Mustang.Name = "Mustang";
+            var Porsche = new Car();
+            Porsche.Name = "Porsche";
+
+            ManualResetEvent car1FinishedEvent = new ManualResetEvent(false);
+            ManualResetEvent car2FinishedEvent = new ManualResetEvent(false);
+
+
+            Thread MustangRace = new Thread(() => Mustang.DriveCar(car1FinishedEvent));
+            Thread PorscheRace = new Thread(() => Porsche.DriveCar(car2FinishedEvent));
+
+
+            MustangRace.Start();
+            PorscheRace.Start();
+
+            Console.WriteLine("Write 'status' to check on the race");
+
+            do
+            {
+                string input = Console.ReadLine();
+                if (input == "status")
+                {
+                    Console.WriteLine($"\n{Mustang.Name}s\nCurrent speed: {Mustang.Speed}\nDistance travelled: {Mustang.KmTravelled.ToString("#.00")}km" +
+                                      $"\n\n{Porsche.Name}s\nCurrent speed: {Porsche.Speed}\nDistance travelled: {Porsche.KmTravelled.ToString("#.00")}km");
+                }
+            } while (Mustang.HaventFinished || Porsche.HaventFinished);
+
+            WaitHandle.WaitAny(new WaitHandle[] { car1FinishedEvent, car2FinishedEvent });
+
+            
+
+            if (car1FinishedEvent.WaitOne(0))
+            {
+                Console.WriteLine("Car 1 wins!");
+            }
+            else
+            {
+                Console.WriteLine("Car 2 wins!");
+            }
+
+            MustangRace.Join();
+            PorscheRace.Join();
+
+            
+        }
+    }
+}
